4f4edb759373cd0c2db1014ee1f0ccd15000012e
btodorov6 <btodorov6@gmail.com>
2025-10-04 15:45:48 +0300

    Add files via upload

---
+++ firstweek/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+int main()
+{
+    int a;
+    int b;
+    cin >> a;
+    cin >> b;
+    cout << (a/b)<<endl;
+    cout << (a % b);
+}
